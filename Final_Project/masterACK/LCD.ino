dce66c442115d48e5963a57862f76f0128de39c0
Mihai Daniel Surcea <mihaidanielsurcea@gmail.com>
2020-02-22 10:33:43 +0200

    master

---
+++ Final_Project/masterACK/LCD.ino
@@ -0,0 +1,479 @@
+#define NAME_LEN 8
+#define MAX_LVL  10
+#define LCD_COLS 16
+bool           lastBtnValue   = !pressed,
+               lockedMeniu    = false, //
+               saveName       = false,
+               waitBtnRls     = false;
+uint16_t       currMsgBit,
+               lastVrxValue   = 0,
+               lastVryValue   = 0;
+const char     endMsg[]       = "Congratulation, you have just died! Press button to continue";
+char           name[NAME_LEN] = "       ";
+int8_t         currLet        = 0,
+               level          = 3,
+               score          = 0;
+uint64_t       currTime,
+               startTime,
+               lastLvlUpTime,
+               startCountDown,
+               lastShown;
+
+static void Lcd_printString (char *str) {
+  uint8_t ch = 0;
+  while (ch < LCD_COLS && str[ch] != '\0') {
+    lcd.print ((char) str[ch++]);
+  }
+  while (ch++ < LCD_COLS) {
+    lcd.print (' ');
+  }
+}
+
+void Lcd_Meniu() {
+  if (!lockedMeniu) {
+    if (vrxValue == left) {
+      if (lastVrxValue != left) {
+        meniuState   = (MeniuState) ((int) meniuState - 1);
+        waitBtnRls   = true;
+        lastVrxValue = left;
+      }
+    }
+    else {
+      if (vrxValue == right) {
+        if (lastVrxValue != right) {
+          meniuState   = (MeniuState) ((int) meniuState + 1);
+          waitBtnRls   = true;
+          lastVrxValue = right;
+        }
+      }
+      else {
+        lastVrxValue = neutral;
+      }
+    }
+  }
+  switch (meniuState) {
+    case MS_Start:
+      lcd.setCursor (0, 0);
+      lcd.print ("BoringGame Meniu");
+      lcd.setCursor (0, 1);
+      // check the btn was really pressed
+      if (btnIsPressed()){
+        if (!waitBtnRls) {
+          if (lastBtnValue == !pressed) {
+            startCountDown = millis();
+            lastBtnValue   = pressed;
+            lockedMeniu    = true;
+            lcd.print (">Start in 3      "); // wait 3s
+            break;
+          }
+          if (millis() > startCountDown + 3000) { // done waiting
+            //Matrix_GameSetup();
+            gameState  = GS_InGame;
+            waitBtnRls = true;
+            lcd.clear();
+          }
+          else {
+            lcd.setCursor (10, 1);
+            if (millis() > startCountDown + 2000) { // wait 1s
+              lcd.print (1);
+            }
+            else {
+              if (millis() > startCountDown + 1000) { // wait 2s
+                lcd.print (2);
+              }
+            }
+          }
+        }
+        else {
+          lcd.print (">Press to start ");
+          lockedMeniu  = false;
+          lastBtnValue = !pressed;
+        }
+      }
+      else {
+        lcd.print (">Press to start ");
+        waitBtnRls   = false;
+        lockedMeniu  = false;
+        lastBtnValue = !pressed;
+      }
+      break;
+    case MS_Login:
+      switch (loginState) {
+        case guest:
+          lcd.setCursor (0, 0);
+          lcd.print ("You are a guest!");
+          lcd.setCursor(0,1);
+          lcd.print (" Press to login ");
+          if (btnIsPressed()){
+            if (!waitBtnRls) {
+               gameState = GS_FingerPrint;
+               loginState = getCode;
+               waitBtnRls = 1;
+            }
+          }
+          else {
+            waitBtnRls   = 0;
+            lastBtnValue = !pressed;
+          }
+          break;
+        case getCode:
+          lcd.setCursor (0, 0);
+          lcd.print ("Enter your code:");
+          lcd.setCursor (0, 1);
+          lcd.print ("(1-39)   ");
+          static uint8_t rsp;
+          rsp = mesageFingerp(1);
+          lcd.print (rsp & 0x7F);
+          lcd.print ("           ");
+          if (rsp > 0x80) {
+            user       = rsp & 0x7F;
+            gameState  = GS_Meniu;
+            loginState = logged;
+            authState  = authed;
+            rsp        = mesageFingerp(0);
+          }
+          break;
+        case logged:
+          lcd.setCursor (0, 0);
+          lcd.print ("Logged is as ");
+          lcd.print (user);
+          lcd.print ("    ");
+          lcd.setCursor (0, 1);
+          lcd.print ("Press to log out");
+          if (btnIsPressed()) {
+            if (!waitBtnRls) {
+              user = 0;
+              loginState = guest;
+              authState  = notAuth;
+              waitBtnRls = 1;
+            }
+          }
+          else {waitBtnRls = 0;}
+          break;
+        default: loginState = guest; break;
+      }
+      break;
+    case MS_Auth:
+      switch (authState) {
+        case notAuth:
+          lcd.setCursor (0, 0);
+          lcd.print ("You are a guest!");
+          lcd.setCursor(0,1);
+          lcd.print ("PressTo register");
+          if (btnIsPressed()){
+            if (!waitBtnRls) {
+               gameState = GS_FingerPrint;
+               authState = setCode;
+               waitBtnRls = 1;
+            }
+          }
+          else {
+            waitBtnRls   = 0;
+            lastBtnValue = !pressed;
+          }
+          break;
+        case setCode:
+          static uint8_t rsp;
+          rsp = mesageFingerp(2);
+          lcd.setCursor (0, 0);
+          lcd.print ("Enter your code:");
+          lcd.setCursor (0, 1);
+          lcd.print ("(1-39)   ");
+          lcd.print (rsp & 0x7F);
+          lcd.print ("           ");
+          if (rsp > 0x80) {
+            user       = rsp & 0x7F;
+            gameState  = GS_Meniu;
+            authState  = authed;
+            loginState = logged;
+            rsp        = mesageFingerp(0);
+          }
+          break;
+        case authed:
+          lcd.setCursor (0, 0);
+          lcd.print ("Logged is as ");
+          lcd.print (user);
+          lcd.print ("    ");
+          lcd.setCursor (0, 1);
+          lcd.print ("Press to log out");
+          if (btnIsPressed()) {
+            if (!waitBtnRls) {
+              user       = 0;
+              authState  = notAuth;
+              loginState = guest;
+              waitBtnRls = 1;
+            }
+          }
+          else {waitBtnRls = 0;}
+          break;
+        default: authState = notAuth; break;
+      }
+      break;
+    case MS_Lvl:
+      lcd.setCursor (0, 0);
+      lcd.print ((lockedMeniu) ? ">Press To UnLock" : ">Press To Lock  ");
+      lcd.setCursor (0, 1);
+      lcd.print ("level: ");
+      lcd.print (level);
+      if (level == 0) {
+        lcd.print (" - MIN");
+      }
+      if (level >= MAX_LVL) {
+        level = MAX_LVL;
+        lcd.print (" - MAX");
+      }
+      lcd.print ("        ");
+      if (btnIsPressed()) {
+        if (lastBtnValue == !pressed) {
+          lockedMeniu  = !lockedMeniu;
+          lastBtnValue = pressed;
+        }
+      }
+      else {
+        lastBtnValue = !pressed;
+      }
+      if (lockedMeniu) {
+        Serial.print(vryValue);
+        if (vryValue == down) {
+          if (lastVryValue != down) {
+            if (level > 0) {--level;}
+            lastVryValue = down;
+          }
+        }
+        else {
+          if (vryValue == up) {
+            if (lastVryValue != up) {
+              if (level < MAX_LVL) {++level;}
+              lastVryValue = up;
+            }
+          }
+          else {
+            lastVryValue = neutral;
+          }
+        }
+      }
+      break;
+    case MS_Hs:
+      static uint8_t rank = 0;
+      char   nameHS[NAME_LEN];
+      lcd.setCursor (0, 0);
+      lcd.print ((lockedMeniu) ? ">Press To UnLock" : ">Press To Lock  ");
+      lcd.setCursor (0, 1);
+      lcd.print ("HS");
+      lcd.print (rank + 1);
+      lcd.print (":           ");
+      lcd.setCursor (5, 1);
+      //lcd.print (HS_Read(rank));
+      lcd.print ("-");
+      //NAME_Read(nameHS, rank);
+      //Lcd_printString (nameHS);
+
+      if (btnIsPressed()) {
+        if (lastBtnValue == !pressed) {
+          lockedMeniu  = !lockedMeniu;
+          lastBtnValue = pressed;
+        }
+      }
+      else {
+        lastBtnValue = !pressed;
+      }
+      if (lockedMeniu) {
+        if (vryValue == down) {
+          if (lastVryValue != down) {
+            //if (rank < HS_NO - 1) {++rank;}
+            lastVryValue = down;
+          }
+        }
+        else {
+          if (vryValue == up) {
+            if (lastVryValue != up) {
+              if (rank > 0) {--rank;}
+              lastVryValue = up;
+            }
+          }
+          else {
+            lastVryValue = neutral;
+          }
+        }
+      }
+      break;
+    case MS_InfoCreator:
+      lcd.setCursor (0, 0);
+      lcd.print ("Creator:  Surcea");
+      lcd.setCursor (0, 1);
+      lcd.print ("Mihai  -  Daniel");
+      break;
+    case MS_InfoGit:
+      lcd.setCursor (0, 0);
+      lcd.print ("   github.com/  ");
+      lcd.setCursor (0, 1);
+      lcd.print ("    Treefold    ");
+      break;
+    case MS_InfoRobotics:
+      lcd.setCursor (0, 0);
+      lcd.print ("  Flappy Birds  ");
+      lcd.setCursor (0, 1);
+      lcd.print ("@UnibucRobotics ");
+      break;
+    case MS_LastState:  meniuState = (MeniuState) ((int) MS_FirstState + 1); break;
+    case MS_FirstState: meniuState = (MeniuState) ((int) MS_LastState  - 1); break;
+    default:            meniuState = (MeniuState) ((int) MS_FirstState + 1); break; 
+  }
+}
+
+void Lcd_EndGame() {
+  lcd.setCursor (0, 0);
+  lcd.print ("Lvl:");
+  //if (level < 10) {lcd.print (" ");} // only one digit
+  //lcd.print (level);
+  lcd.print (" Score:");
+  //if (score < 100) {lcd.print (" ");} // less than 3 digits
+ // lcd.print (score);
+ // if (score < 10) { lcd.print (" ");} // only one digit
+  lcd.setCursor (0, 1);
+  if (millis() > lastShown + 500) { // refresh rate
+    lastShown = millis();
+    if (endMsg[currMsgBit] == '\0') {currMsgBit = 0;} // reprint the message
+    Lcd_printString ((char*)endMsg + (currMsgBit++));
+  }
+  if (btnIsPressed()) {
+    if (!waitBtnRls) {
+      waitBtnRls = true;
+      //if (score > HS_Read(HS_NO - 1)) { // last rank
+        //saveName   = false;
+        //gameState  = GS_Hs;
+      //}
+     // else {
+        gameState  = GS_Meniu;
+        meniuState = MS_Start;
+     //}
+    }
+  }
+  else {waitBtnRls = false;}
+}
+
+void Lcd_InGame() {
+  lcd.setCursor (0, 0);
+  //lcd.print ("Lives: ");lcd.print (lives);
+ // lcd.print (" Lvl: "); lcd.print (level);
+  lcd.setCursor (0, 1);
+  lcd.print ("TIME: ");
+  //if (level < MAX_LVL) {
+    //if ((uint16_t) remTime < 10) {lcd.print (" ");}
+    //lcd.print ((uint16_t) remTime);
+  //}
+ // else {lcd.print ("INF");}
+  lcd.print (" SC: ");
+  //lcd.print (score);
+}
+
+void Lcd_HsSaveName() {
+  lcd.setCursor (6, 0);
+  for (uint8_t currCh = 0; currCh < NAME_LEN - 1; ++currCh) { // -1 for the last char is '\0'
+    if (currCh == currLet) {
+      lcd.print ('>');
+      lcd.print (name[currCh]);
+      lcd.print ('<');
+    }
+    else {lcd.print (name[currCh]);}
+  }
+  
+  lcd.setCursor (0, 1);
+  if (btnIsPressed()) {
+    if (!waitBtnRls) {
+      if (lastBtnValue == !pressed) {
+        lcd.setCursor (0, 1);
+        lcd.print (">Name saved in 3");
+        startCountDown = millis();
+        lockedMeniu    = true;
+        lastBtnValue   = pressed;
+      }
+      if (millis() > startCountDown + 3000) {
+       //HS_Update (score, name);
+        gameState   = GS_Meniu;
+        meniuState  = MS_Start;
+        saveName    = false;
+        lockedMeniu = false;
+        waitBtnRls  = true;
+      }
+      else {
+        lcd.setCursor (15, 1);
+        if (millis() > startCountDown + 2000) {
+          lcd.print (1);
+        }
+        else {
+          if (millis() > startCountDown + 1000) {
+            lcd.print (2);
+          }
+        }
+      }
+      return;
+    }
+  }
+  else {
+    lcd.print (" Press To Save  ");
+    waitBtnRls   = false;
+    lastBtnValue = !pressed;
+    lockedMeniu  = false;
+  }
+  
+  if (!lockedMeniu) {
+    if (vrxValue == left) {
+      if (lastVrxValue != left && vryValue == up && vryValue == down && currLet > 0) {
+        currLet--;
+        lastVrxValue = left;
+        lastVryValue = neutral;
+      }
+    }
+    else {
+      if (vrxValue == right) {
+        if (lastVrxValue != right && vryValue == up && vryValue == down && currLet < NAME_LEN - 2) {
+          // -2 for each: the last one is '\0' and cannot move to the right letter if there are no more letters
+          currLet++;
+          lastVrxValue = right;
+          lastVryValue = neutral;
+        }
+      }
+      else {
+        lastVrxValue = neutral;
+        if (vryValue == down) {
+          if (lastVryValue != down) {
+            lastVryValue = down;
+            if (name[currLet] == ' ') {
+              name[currLet] = 'Z';
+            }
+            else {
+              if (name[currLet] == 'A') {
+                name[currLet] = ' ';
+              }
+              else {
+                name[currLet]--;
+              }
+            }
+          }
+        }
+        else {
+          if (vryValue == up) {
+            if (lastVryValue != up) {
+              lastVryValue = up;
+              if (name[currLet] == ' ') {
+                name[currLet] = 'A';
+              }
+              else {
+                if (name[currLet] == 'Z') {
+                  name[currLet] = ' ';
+                }
+                else {
+                  name[currLet]++;
+                }
+              }
+            }
+          }
+          else {
+            lastVryValue = neutral;
+          }
+        }
+      }
+    }
+  }
+}
--- Final_Project/masterACK/masterACK.ino
+++ Final_Project/masterACK/masterACK.ino
@@ -1,29 +1,44 @@
+// Game states and meniu states can be change anywhere
+// This file must have the name of its containing file
 #include <LiquidCrystal_I2C.h>
 #include <SPI.h>
 #include "nRF24L01.h"
 #include "RF24.h"
 #include "printf.h"
-#define  PIN const uint8_t
+#define  PIN const uint8_t    
+#define neutral 0
+#define left    1
+#define right   2
+#define down    1
+#define up      2
+#define pressed 1
 PIN cePin           = 9, // rf24 cip enable
     csnPin          = 8, // rf24 cip select not
     incomingDataPin = 2; // rf24 interrupt signal
-// too many pins to declare each of them as a define
-LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  
-RF24 radio(cePin, csnPin);  // CE, CSN
-byte addresses[][6] = {"SSlve", "SMstr"};
-struct Msg {uint8_t id, nr;} msg;
+    
+uint8_t vrxValue = neutral,
+        vryValue = neutral,
+        user;
+bool    btnValue = !pressed;
 
-void setup() {
-  Serial.begin(9600);
-  lcd.begin(16,2); 
-  lcd.clear(); 
-  radio.begin();
-  radio.enableAckPayload();
-  radio.enableDynamicPayloads();
-  radio.stopListening();
-  radio.openWritingPipe(addresses[0]);
-  radio.openReadingPipe(1, addresses[1]);
-}
+enum GameStates : uint8_t { // GS
+  GS_Meniu,
+  GS_FingerPrint,
+  GS_InGame
+} gameState = GS_Meniu;
+
+enum MeniuState : uint8_t {
+  MS_FirstState, // link to the previous of the last state
+  MS_Start,
+  MS_Login,
+  MS_Auth,
+  MS_Lvl,
+  MS_Hs,
+  MS_InfoCreator,
+  MS_InfoGit,
+  MS_InfoRobotics,
+  MS_LastState   // link to the next of the first state
+} meniuState = MS_Start;
 
 enum {
   logIn,
@@ -33,39 +48,85 @@ enum {
 } masterState = logIn;
 
 enum {
-  
+  guest, // ast to not remain a guest
+  getCode,
+  logged
+} loginState = guest;
+
+enum {
+  notAuth,
+  setCode,
+  authed
+} authState = notAuth;
+
+// too many pins to declare each of them as a define
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  
+RF24 radio(cePin, csnPin);  // CE, CSN
+byte addresses[][6] = {"SSlve", "SMstr", "1Slve", "1Mstr"};
+struct Msg {uint8_t id, nr;} msg;
+
+void Askjs() { // ask joystick for its states: vrX, vrY, brn
+  //static uint32_t lastJsMsg = 0;
+  //if (lastJsMsg + 50 < millis()) {return 0;}
+  //lastJsMsg = millis();
+  msg.id = '1';
+  msg.nr = 0x80;
+  radio.openWritingPipe(addresses[2]);
+  if (radio.write(&msg, sizeof(Msg))) { 
+      while (radio.available() ) { 
+        radio.read( &msg, sizeof(Msg));  
+        //Serial.println(msg.nr, BIN);   
+        vrxValue = msg.nr & 3;
+        vryValue = (msg.nr & (3<<2)) >> 2;
+        btnValue = (msg.nr & (1<<4)) >> 4;
+      }
+  } else {
+    Serial.println("Sending failed."); 
+  }
 }
 
-void mesage() {
+bool btnIsPressed () {
+  return btnValue == pressed;
+}
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("start");
+  lcd.begin(16,2); 
+  lcd.clear(); 
+  radio.begin();
+  radio.enableAckPayload();
+  radio.enableDynamicPayloads();
+  radio.stopListening();
+  radio.openWritingPipe(addresses[0]);
+  radio.openReadingPipe(1, addresses[1]);
+  radio.openReadingPipe(2, addresses[2]);
+}
+
+uint8_t mesageFingerp(uint8_t st) {
   msg.id = 'S';
-    msg.nr = 1;
-    if ( radio.write(&msg, sizeof(Msg)) ) { 
-      
-        while (radio.available() ) {                    // If an ack with payload was received
-          radio.read( &msg, sizeof(Msg));                  // Read it, and display the response time
-          unsigned long timer = micros();
-
-          Serial.print(F("Got response "));
-          Serial.println(msg.nr);                                 // Increment the counter variable
-        }
-      
-
-    } else {
-      Serial.println(F("Sending failed."));  // If no ack response, sending failed
-    }
-}
-
-void doLogIn() {
-  lcd.setCursor(0, 0);
-  lcd.print ("   Guest   LogIn");
-  lcd.setCursor(0, 1);
-  lcd.print ("   CreateNew    ");
-  
+  msg.nr = st;
+  radio.openWritingPipe(addresses[0]);
+  if ( radio.write(&msg, sizeof(Msg)) ) {
+      msg.id = 0;
+      while (radio.available()) {
+        radio.read( &msg, sizeof(Msg)); 
+        if(msg.id != 'S') {Serial.println(msg.id);continue;} 
+        return msg.nr;
+      }
+  } else {
+    Serial.println("Sending failed");  // If no ack response, sending failed
+  }
+  return 1;
 }
 
 void loop(void) {
-  switch(masterState) {
-    case logIn: doLogIn();
-      
+  switch (gameState) {
+    case GS_Meniu: Askjs(); break;
+    case GS_FingerPrint: break;
+    case GS_InGame: break;
+    default: gameState = GS_Meniu; break;
   }
+  Lcd_Meniu(); 
 }
